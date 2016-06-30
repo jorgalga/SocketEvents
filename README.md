9d6e98f85c4346825f7e4b0c6db7f082d23282eb
Jorge Gálvez <jorge.galvez@b-reel.com>
2016-06-30 12:36:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# SocketEvents
+
+This is the fist part of an experiment to develop a web-based controller for webVR applications.
+
+Install first node.js and socket.io with npm (SUDO permission):
+$ npm install socket.io
+
+Run the app:
+$ node app.js
+
+open in a browser index.html and interact with the keyboard
---
+++ app.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8888);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+  socket.on('keydown', function (data) {
+      data = JSON.parse(data);
+      console.log(data.keydown);
+
+  });
+
+
+});
